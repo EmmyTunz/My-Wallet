c8a9d18bb9ea1721243086302aaab0ea5b7d2479
EmmyTunz <tummagold@gmail.com>
2026-06-01 12:13:09 +0100

    Add files via upload

---
+++ My Wallet/transaction.py
@@ -0,0 +1,12 @@
+class Transaction:
+    def __init__(self):
+        self.amount = 0
+        self.type = 0
+        self.category = 0
+        self.date = 0
+        self.note = ""
+
+    def save_transaction(self):
+        pass
+
+
