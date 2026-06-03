7884fafc5329fe968684f897484051c456bed383
EmmyTunz <tummagold@gmail.com>
2026-06-03 12:07:12 +0100

    I solved a few issues, changed the date format, added the transaction id, catch the error where Transaction.json is empty, started the working on the table

--- main.py
+++ main.py
@@ -1,55 +1,69 @@
 import pandas as pd
 import json
 from datetime import datetime
+from prettytable import PrettyTable
 
 new_transaction = True
 transaction_list = []
 
-
-
+# Load existing data from previous sessions.
 try:
     with open("Transaction.json", "r") as data_file:
         transaction_file = json.load(data_file)
-        print(transaction_file)
         for key in transaction_file["Transaction"]:
             key = transaction_file["Transaction"][key]
             transaction_list.append({
                 "Transaction": {
+                    "id": key["id"],
                     "amount" : key["amount"],
                     "type" : key["type"],
                     "date" : key["date"],
                     "note" : key["note"],
         }})
-        print(transaction_list)
+        # get the transaction ID of the previous transaction and add 1 to it for the next transaction
+        transaction_id = int(list(transaction_file["Transaction"].keys())[len(transaction_file["Transaction"].keys()) - 1]) + 1
 
-except FileNotFoundError:
+except FileNotFoundError, json.decoder.JSONDecodeError:
     transaction_list = []
+    transaction_id = 0
 
 
-
+# add transaction to table
 while new_transaction:
     transaction_amount = int(input("Enter Transaction Amount in digits "))
-    transaction_type = input("Enter Transaction Type Income/Expense ")
-    category = input("Enter the Category of this transaction ")
+    transaction_type = input("Enter Transaction Type Income/Expense ").capitalize()
+    category = input("Enter the Category of this transaction ").capitalize()
     date = datetime.now()
     note = input("Enter a description ")
-    new_transaction = input("New transaction? (True/False) ")
-    if new_transaction == "False":
-        new_transaction = False
 
     transaction_dict = {
         "Transaction": {
+            "id": transaction_id,
             "amount" : transaction_amount,
             "type" : transaction_type,
-            "date" : date,
+            "date" : date.strftime("%d/%m/%Y %H:%M"),
             "note" : note,
         }
     }
     transaction_list.append(transaction_dict)
+
+    new_transaction = input("New transaction? (True/False) ").capitalize()
+    if new_transaction == "False":
+        new_transaction = False
+    elif new_transaction == "True":
+        new_transaction = "True"
+        transaction_id += 1
+    else:
+        new_transaction = input("is there a new transaction? type True or False ").capitalize()
+
+# Display Transaction Table
+transaction_table = PrettyTable(["Transaction_ID", "Amount", "Type", "Category", "Date", "Note"])
+# for i in transaction_list:
+
+
 data = pd.DataFrame(transaction_list)
 data.to_json("Transaction.json")
 print(transaction_list)
 print(data)
 
 
-# add transaction to table
--- transaction.py
+++ transaction.py
@@ -13,3 +13,12 @@ class Transaction:
         pass
 
 
+
+transactions = {"Transaction":{"0":{"amount":10000,"type":"Income","date":1780322240784,"note":"Sweet Sensation"},"1":{"amount":20000,"type":"expenses","date":1780322269518,"note":"wWWss"},"2":{"amount":100000,"type":"Income","date":1780482364566,"note":"thank God"},"3":{"amount":10000,"type":"Expense","date":1780482403690,"note":"Gift data"},"4":{"amount":10000,"type":"Income","date":1780482797304,"note":"May"},"5":{"amount":100000,"type":"expense","date":1780482864701,"note":"benz"},"6":{"amount":2000,"type":"ecpense","date":1780483275806,"note":"eggroll"},"7":{"amount":3000,"type":"expense","date":1780483303549,"note":"gift for sister"}}}
+
+del transactions["Transaction"]["1"]
+print(transactions)
+
+# keys = list(transactions["Transaction"].keys())
+# print(keys)
+# print(keys[len(transactions["Transaction"].keys()) - 1])
